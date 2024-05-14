45c273f69529536823290874d8b769c57dd4b6f5
Eduardo Gonzalez Melgoza <edu.glz.mlz@gmail.com>
2024-05-14 13:33:37 +0200

    Initial commit

---
+++ MCL/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  MCL
+//
+//  Created by Eduardo Gonzalez Melgoza on 14/05/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+
+    var body: some View {
+        Text("hi")
+    }
+}
