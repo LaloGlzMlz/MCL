ea7b86437c152a8447c96067bdb1585ca7a04c11
Eduardo Gonzalez Melgoza <edu.glz.mlz@gmail.com>
2024-05-17 16:19:44 +0200

    Save before cleaning up interfaces

--- MCL/FirstScreenView.swift
+++ MCL/FirstScreenView.swift
@@ -10,48 +10,47 @@ struct FirstScreenView: View {
     @State var Album: AlbumModel?
     @State var albumTitle: String = ""
     @State var name: String?
+    
     var AlbumArray: [AlbumModel] = [AlbumModel(id: UUID(), image: Image("street"), name: "Last Sunday", date: Date()), AlbumModel(id: UUID(), image: Image("metro"), name: "Last Monday", date: Date()), AlbumModel(id: UUID(), image: Image("man"), name: "Last Kitemmuort", date: Date()), AlbumModel(id: UUID(), image: Image("vesuvio"), name: "Last Suca", date: Date()), AlbumModel(id: UUID(), image: Image("sea"), name: "Last Paolo", date: Date()),]
+    
     
     var body: some View {
         NavigationStack {
             VStack(alignment: .center) {
                 ScrollView(.horizontal, showsIndicators: false) {
-                    VStack {
-                        HStack(alignment: .center){
-                            ForEach(AlbumArray) { album in
-                                VStack {
-                                    NavigationLink(destination: AlbumDetailView(album: album)) {
-                                        album.image
-                                            .resizable()
-                                            .frame(width: 200, height: 200)
-                                            .padding(.horizontal, 10)
-                                            .cornerRadius(15)
-                                            .scrollTransition (topLeading: .interactive, bottomTrailing: .interactive, axis: .horizontal) { effect, phase in
-                                                effect
-                                                    .scaleEffect(1.5 - abs(phase.value))
-                                                    .opacity(1 - abs(phase.value))
-                                                    .rotation3DEffect(
-                                                        .degrees(phase.value / 90),
-                                                        axis: (x: 0, y: 1, z: 0)
-                                                    )
-                                            }
-                                            .padding(.horizontal, 0)
-                                            .padding(.vertical)
-                                    }
-                                    Text(album.name)
-                                        .padding(.top, 50)
-                                    
-                                    Text(album.date.formatted(date: .abbreviated, time: .omitted))
-                                    
+                    HStack(alignment: .center){
+                        ForEach(AlbumArray) { album in
+                            VStack {
+                                NavigationLink(destination: AlbumDetailView(album: album)) {
+                                    album.image
+                                        .resizable()
+                                        .frame(width: 200, height: 200)
+                                        .padding(.horizontal, 10)
+                                        .cornerRadius(15)
+                                        .scrollTransition (topLeading: .interactive, bottomTrailing: .interactive, axis: .horizontal) { effect, phase in
+                                            effect
+                                                .scaleEffect(1.5 - abs(phase.value))
+                                                .opacity(1 - abs(phase.value))
+                                                .rotation3DEffect(
+                                                    .degrees(phase.value / 90),
+                                                    axis: (x: 0, y: 1, z: 0)
+                                                )
+                                        }
+                                        .padding(.horizontal, 0)
+                                        .padding(.vertical)
                                 }
+                                Text(album.name)
+                                    .padding(.top, 50)
+                                
+                                Text(album.date.formatted(date: .abbreviated, time: .omitted))
+                                
                             }
-                            .padding(.bottom, 50)
                         }
-                        .scrollTargetLayout()
-                        .padding(.leading, 60)
-                        .padding(.trailing, 50)
+                        .padding(.bottom, 50)
                     }
-                    
+                    .scrollTargetLayout()
+                    .padding(.leading, 60)
+                    .padding(.trailing, 50)
                 }
                 .safeAreaPadding(.horizontal, 25)
                 .scrollTargetBehavior(.viewAligned)
