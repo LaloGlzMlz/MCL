07403167831f767f3e9b887dc2b43b019a94c4e9
Michel Andre Pellegrin Quiroz <michelpellegrin04@gmail.com>
2024-05-21 23:21:50 +0200

    Add album cover and title to the booklet view

--- MCL/Views/BookletView.swift
+++ MCL/Views/BookletView.swift
@@ -17,29 +17,57 @@ struct BookletView: View {
     
     var body: some View {
         NavigationStack{
-                ForEach(album.songs){ song in
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 5)
-                            .frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height/12)
-                            .foregroundColor(.white)
-                            .shadow(color: Color.black.opacity(0.15), radius: 20)
-                        HStack{
-                            AsyncImage(url: song.imageURL)
-                                .frame(width: 40, height: 40, alignment: .leading)
-                                .padding()
-                            VStack(alignment: .leading) {
-                                Text(song.name)
-                                    .fontWeight(.medium)
-                                    .lineLimit(1)
-                                Text(song.artist)
-                                    .font(.footnote)
-                                    .fontWeight(.light)
+            ScrollView{
+                AlbumCard(album: album)
+                VStack(alignment: .leading){
+                    Text(album.title)
+                        .font(.title)
+                        .bold()
+                    //Description
+                    Text("Placeholder for description about the album")
+                        .font(.subheadline)
+                    ForEach(album.songs){ song in
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 5)
+                                .frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height/12)
+                                .foregroundColor(.white)
+                                .shadow(color: Color.black.opacity(0.15), radius: 20)
+                            HStack{
+                                AsyncImage(url: song.imageURL)
+                                    .frame(width: 40, height: 40, alignment: .leading)
+                                    .padding()
+                                VStack(alignment: .leading) {
+                                    Text(song.name)
+                                        .fontWeight(.medium)
+                                        .lineLimit(1)
+                                    Text(song.artist)
+                                        .font(.footnote)
+                                        .fontWeight(.light)
+                                }
                             }
+                            .frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height/11, alignment: .leading)
                         }
-                        .frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height/11, alignment: .leading)
                     }
                 }
-            .navigationTitle(album.title)
+                .toolbar {
+                    ToolbarItemGroup(placement: .topBarTrailing) {
+                        Button(action: {
+                            //Here action
+                        }) {
+                            Label("Share",systemImage: "square.and.arrow.up")
+                        }
+                    }
+                    ToolbarItemGroup(placement: .topBarTrailing) {
+                        Button(action: {
+                            //Here action
+                        }) {
+                            Label("Options",systemImage: "ellipsis")
+                        }
+                    }
+                }
+            }
+            .scrollIndicators(.hidden)
+            
         }
     }
 }
