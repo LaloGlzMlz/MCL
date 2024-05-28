3d67e182e59a70d175e79448d95dd070a6d80009
IlSense98 <f.sensenhauser@gmail.com>
2024-05-28 15:04:20 +0200

    Artwork average color

--- MCL/Views/BookletView.swift
+++ MCL/Views/BookletView.swift
@@ -18,6 +18,7 @@ struct BookletView: View {
     @StateObject private var songStore = SongStore()
     @State private var showConfirmationDialog = false
     @State private var showingEditAlbumSheet: Bool = false
+    @State private var averageColor: Color = .primary
     
     var body: some View {
         //        NavigationStack { DO NOT PUT NAVIGATION STACK ON THIS VIEW, NEVEEEER!!!!
@@ -39,7 +40,7 @@ struct BookletView: View {
                             .foregroundStyle(Color.gray)
                             .font(.footnote)
                             .bold()
-//                            .padding()
+                        //                            .padding()
                     }
                     if album.dateTo != nil {
                         HStack {
@@ -65,28 +66,9 @@ struct BookletView: View {
                         .foregroundStyle(Color.gray)
                         .font(.subheadline)
                     ForEach($album.songs) { $song in
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .foregroundColor(.white)
-                                .shadow(color: Color.black.opacity(0.15), radius: 20)
-                                .frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height/12)
-                            
-                            HStack {
-                                AsyncImage(url: song.imageURL)
-                                    .frame(width: 40, height: 40, alignment: .leading)
-                                    .padding()
-                                
-                                VStack(alignment: .leading) {
-                                    Text(song.name)
-                                        .fontWeight(.medium)
-                                        .lineLimit(1)
-                                    
-                                    Text(song.artist)
-                                        .font(.footnote)
-                                }
-                            }
-                            .frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height/11, alignment: .leading)
-                        }
+                        SongView(song: song)
+                            .frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height/12)
+                            .shadow(color: Color.black.opacity(0.15), radius: 20)
                     }
                 }
             }
@@ -113,9 +95,9 @@ struct BookletView: View {
             Button("Add Entry") {
                 // Action 1
             }
-//            Button("Add...") {
-//
-//            }
+            //            Button("Add...") {
+            //
+            //            }
             Button("Cancel", role: .cancel) {
                 // Cancel action
             }
@@ -123,6 +105,24 @@ struct BookletView: View {
         .sheet(isPresented: $showingEditAlbumSheet) {
             EditAlbumView(album: album)
         }
+        .onAppear {
+            
+        }
     }
+    private func calculateAverageColor(from image: UIImage) -> Color? {
+        guard let cgImage = image.cgImage else { return nil }
+        let ciImage = CIImage(cgImage: cgImage)
+        let extentVector = CIVector(x: ciImage.extent.origin.x, y: ciImage.extent.origin.y, z: ciImage.extent.size.width, w: ciImage.extent.size.height)
+        
+        guard let filter = CIFilter(name: "CIAreaAverage", parameters: [kCIInputImageKey: ciImage, kCIInputExtentKey: extentVector]) else { return nil }
+        guard let outputImage = filter.outputImage else { return nil }
+        
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        let context = CIContext(options: [.workingColorSpace: kCFNull as Any])
+        context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
+        
+        return Color(red: Double(bitmap[0]) / 255.0, green: Double(bitmap[1]) / 255.0, blue: Double(bitmap[2]) / 255.0, opacity: Double(bitmap[3]) / 255.0)
+    }
+    
+    
 }
-
