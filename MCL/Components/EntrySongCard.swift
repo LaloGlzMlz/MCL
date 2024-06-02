a1e6aa539e5d30aaf028105143ead1e1371d2d2a
Eduardo Gonzalez Melgoza <edu.glz.mlz@gmail.com>
2024-06-02 15:11:52 +0200

    song entry cards almost ready, just need prompt to wrap into next line

--- MCL/Components/EntrySongCard.swift
+++ MCL/Components/EntrySongCard.swift
@@ -52,7 +52,6 @@ struct EntrySongCard: View {
                                     .scaledToFit()
                                     .frame(width: 55, height: 55)
                                     .clipShape(RoundedRectangle(cornerRadius: 6))
-                                    .padding(.top, 10)
                                     .padding(.trailing, 5)
                                     .onAppear {
                                         // Fetch the color when the image appears
@@ -93,14 +92,9 @@ struct EntrySongCard: View {
                 VStack {
                     ForEach(song.entries) { entry in
                         ZStack {
-                            GeometryReader { geometry in
-                                RoundedRectangle(cornerRadius: 5)
-                                    .foregroundColor(.white)
-                                    .opacity(0.1)
-                                    .padding([.bottom, .trailing], 10)
-                                    .frame(height: geometry.size.height)
-                            }
-//                            .frame(width: UIScreen.main.bounds.width / 1.1)
+                            RoundedRectangle(cornerRadius: 5)
+                                .foregroundColor(.white)
+                                .opacity(0.1)
                             
                             VStack {
                                 if entry.prompt != "" {
@@ -109,19 +103,25 @@ struct EntrySongCard: View {
                                         .frame(maxWidth: .infinity, alignment: .leading)
                                         .foregroundStyle(Color.white)
                                         .bold()
+                                        .lineLimit(nil) // Allow text to wrap
+                                        .multilineTextAlignment(.leading) // Ensure text aligns to the left
                                     Divider()
                                         .padding(.horizontal)
                                 }
                                 Text(entry.entryText)
-                                    .padding()
+                                    .padding(10)
                                     .foregroundStyle(Color.white)
                                     .frame(maxWidth: .infinity, alignment: .leading)
+                                    .lineLimit(nil) // Allow text to wrap
+                                    .multilineTextAlignment(.leading) // Ensure text aligns to the left
                             }
                         }
+                        .padding(.bottom, 10) // Add padding to separate each entry
                     }
                 }
+                .padding(.horizontal, 10) // Ensure VStack does not clip its content
             }
-            .padding(.leading, 10) // Add padding to the VStack to create space around the content
+            .padding(10) // Add padding to the VStack to create space around the content
         }
         .frame(width: UIScreen.main.bounds.width / 1.1)
         .fixedSize(horizontal: false, vertical: true) // Allow the ZStack to resize vertically based on content
