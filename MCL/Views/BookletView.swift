14ba366e255a65f433149e210d3056c287d0a2c4
Francesca Ferrini <francesca_ferrini@hotmail.com>
2024-06-03 14:53:19 +0200

    FullScreen done

--- MCL/Views/BookletView.swift
+++ MCL/Views/BookletView.swift
@@ -6,10 +6,10 @@
 //
 
 import SwiftUI
-import SwiftData
-
 struct BookletView: View {
     @Environment(\.modelContext) private var context
+    
+    @Environment(\.dismiss) var dismiss
     
     @State private var refreshList = false
     @State private var songsFromAlbum: [SongStore] = []
@@ -21,99 +21,137 @@ struct BookletView: View {
     @State private var isShowingEditView = false
     @State private var isShowingNewEntryView = false
     
+    @State private var navigationBarColor: Color = .clear
+    @State private var navigationBarTitleOpacity: Double = 0
+    
     @Bindable var album: Album
     
-   
     var body: some View {
-        VStack {
-            GeometryReader { geometry in
-                ZStack(alignment: .topLeading) {
+        ScrollView {
+            VStack {
+                GeometryReader { geometry in
+                    Color.red.opacity(0.5)
+                    let minY = geometry.frame(in: .global).minY
+                    let scale = 1 + minY / 350
+                    let offset = minY > 0 ? -minY : 0
+                    
                     AlbumCard(album: album, isExpanded: true)
-                        .frame(height: geometry.size.height * 0.5)
+                        .frame(height: 350)
+                        .scaleEffect(minY > 0 ? scale : 1, anchor: .center)
+                        .offset(y: offset)
+                        .onChange(of: minY) { newValue in
+                            if newValue < -150 {
+                                withAnimation {
+                                    navigationBarColor = .white
+                                    navigationBarTitleOpacity = 1
+                                }
+                            } else {
+                                withAnimation {
+                                    navigationBarColor = .clear
+                                    navigationBarTitleOpacity = 0
+                                }
+                            }
+                        }
                 }
+                .frame(height: 350)
             }
-            // NavigationStack { DO NOT PUT NAVIGATION STACK ON THIS VIEW, NEVEEEER!!!!
-            // Stack per i dettagli dell'album
-            ScrollView {
-                VStack(alignment: .leading, spacing: 10) {
-                    if album.location != "" {
-                        Text(album.location)
+            .ignoresSafeArea()
+            .padding(.bottom)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                if album.location != "" {
+                    Text(album.location)
+                        .foregroundStyle(Color.gray)
+                        .font(.footnote)
+                        .bold()
+                }
+                if album.dateTo != nil {
+                    HStack {
+                        Text(album.dateFrom!, style: .date)
                             .foregroundStyle(Color.gray)
                             .font(.footnote)
-                            .bold()
+                        Text("-")
+                            .foregroundStyle(Color.gray)
+                            .font(.footnote)
+                        Text(album.dateTo!, style: .date)
+                            .foregroundStyle(Color.gray)
+                            .font(.footnote)
                     }
-                    if album.dateTo != nil {
-                        HStack {
-                            Text(album.dateFrom!, style: .date)
-                                .foregroundStyle(Color.gray)
-                                .font(.footnote)
-                            Text("-")
-                                .foregroundStyle(Color.gray)
-                                .font(.footnote)
-                            Text(album.dateTo!, style: .date)
-                                .foregroundStyle(Color.gray)
-                                .font(.footnote)
-                        }
-                    } else if album.dateFrom != nil && album.dateTo == nil {
-                        HStack {
-                            Text(album.dateFrom!, style: .date)
-                                .foregroundStyle(Color.gray)
-                                .font(.footnote)
-                        }
-                    }
-                    
-                    Text(album.shortDescription)
-                        .foregroundStyle(Color.gray)
-                        .font(.subheadline)
-
-                    Divider()
-                        .padding()
-                    
-                    ForEach(album.entries) { entry in
-                        ZStack {
-                            GeometryReader { geometry in
-                                RoundedRectangle(cornerRadius: 5)
-                                    .foregroundColor(.white)
-                                    .shadow(color: Color.black.opacity(0.15), radius: 20)
-                                    .frame(height: geometry.size.height)
-                            }
-                            .frame(width: UIScreen.main.bounds.width / 1.1)
-                            
-                            Text(entry.entryText)
-                                .padding()
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        
-                    }
-                    
-                    ForEach($album.songs) { $song in
-                        SongCard(song: song)
-                            .frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height/12)
-                            .shadow(color: Color.black.opacity(0.15), radius: 20)
+                } else if album.dateFrom != nil && album.dateTo == nil {
+                    HStack {
+                        Text(album.dateFrom!, style: .date)
+                            .foregroundStyle(Color.gray)
+                            .font(.footnote)
                     }
                 }
                 
-            } .padding(.horizontal)
+                Text(album.shortDescription)
+                    .foregroundStyle(Color.gray)
+                    .font(.subheadline)
+                
+                Divider()
+                    .padding()
+                
+                ForEach(album.entries) { entry in
+                    ZStack {
+                        GeometryReader { geometry in
+                            RoundedRectangle(cornerRadius: 5)
+                                .foregroundColor(.white)
+                                .shadow(color: Color.black.opacity(0.15), radius: 20)
+                                .frame(height: geometry.size.height)
+                        }
+                        .frame(width: UIScreen.main.bounds.width / 1.1)
+                        
+                        Text(entry.entryText)
+                            .padding()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+ForEach($album.songs) { $song in
+                    SongCard(song: song)
+                        .frame(width: UIScreen.main.bounds.width / 1.1, height: UIScreen.main.bounds.height / 12)
+                        .shadow(color: Color.black.opacity(0.15), radius: 20)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 60)
             
+            
+            Spacer()
+            
+                .background(navigationBarColor)
+                .toolbar { 
+                    ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image(systemName: "lessthan.circle.fill")
+                            .foregroundStyle(.white, .gray)
+                    }
+                }
+                    ToolbarItemGroup(placement: .topBarTrailing) {
+                        Button(action: {
+                            showingEditAlbumSheet = true
+                        }) {
+                            Label("Edit", systemImage: "pencil.circle.fill")
+                        }
+                        .foregroundStyle(.white, .gray)
+                    }
+                    ToolbarItemGroup(placement: .topBarTrailing) {
+                        Button(action: {
+                            showConfirmationDialog = true
+                        }) {
+                            Label("Options", systemImage: "plus.circle.fill")
+                        }
+                        .foregroundStyle(.white, .gray)
+                    }
+                }
+                .toolbarBackground(navigationBarColor.opacity(0.3), for: .navigationBar)
+                .navigationBarBackButtonHidden(true)
+                .navigationBarTitleDisplayMode(.inline)
+                .opacity(navigationBarTitleOpacity)
         }
-        //.navigationTitle(album.title)
-        .toolbar {
-            ToolbarItemGroup(placement: .topBarTrailing) {
-                Button(action: {
-                    showingEditAlbumSheet = true
-                }) {
-                    Label("Edit", systemImage: "pencil")
-                }.accentColor(.gray)
-            }
-            ToolbarItemGroup(placement: .topBarTrailing) {
-                Button(action: {
-                    showConfirmationDialog = true
-                }) {
-                    Label("Options", systemImage: "plus.circle.fill")
-                        .tint(.gray)
-                }.accentColor(.gray)
-            }
-        }
+        .ignoresSafeArea()
         .confirmationDialog("", isPresented: $showConfirmationDialog, titleVisibility: .hidden) {
             Button(action: {
                 isShowingNewEntryView = true
@@ -130,10 +168,8 @@ struct BookletView: View {
         .sheet(isPresented: $showingEditAlbumSheet) {
             EditAlbumView(album: album)
         }
-        .onAppear {
-            
-        }
     }
+    
     private func calculateAverageColor(from image: UIImage) -> Color? {
         guard let cgImage = image.cgImage else { return nil }
         let ciImage = CIImage(cgImage: cgImage)
@@ -148,6 +184,4 @@ struct BookletView: View {
         
         return Color(red: Double(bitmap[0]) / 255.0, green: Double(bitmap[1]) / 255.0, blue: Double(bitmap[2]) / 255.0, opacity: Double(bitmap[3]) / 255.0)
     }
-    
-    
 }
